8f1f3b9673078e67003feb816e162eabb349b0ac
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2016-10-15 17:23:50 +0500

    Commit app structure

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import KanbanBoard from './components/KanbanBoard';
+
+
+let cardsList = [
+	{
+		id: 1,
+		title: 'Read the Book',
+		description: 'I should read the **whole** book',
+		color: '#BD8D31',
+		status: 'in-progress',
+		tasks: []
+	},
+
+	{
+		id: 2,
+		title: 'Write some code',
+		description: 'Code along with the sample in the book. \
+			The complete source can be found at [github](https://github.com/pro-react)',
+		color: '#3A7E28',
+		status: 'todo',
+		tasks: [
+			{
+				id: 1,
+				name: 'ContactList Example',
+				done: true
+			},
+			{
+				id: 2,
+				name: 'Kanban Example',
+				done: false
+			},
+			{
+				id: 3,
+				name: 'My own exeriments',
+				done: false
+			}
+		]
+	}
+];
+
+ReactDOM.render(<KanbanBoard cards={cardsList} />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require("webpack-html-plugin");
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: __dirname + "/src/js/app.js",
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('style', 'css?modules')
+			},
+			{
+		        test: /\.(jpe?g|png|gif|svg)$/i,
+		        loaders: [
+		            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+		            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+		        ]
+		    }
+		]
+	},
+	plugins: [HtmlWebpackPluginConfig,
+			  new ExtractTextPlugin('style.css')
+			 ]
+
+}
