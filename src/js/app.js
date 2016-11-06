8b80ea41724553981714bc2d550098a0f018fc79
Rehan Ahmed <Rehan.ahmed19@outlook.com>
2016-11-06 14:34:17 +0500

    Add the routes to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {Router, Route, hashHistory} from 'react-router';
 import KanbanBoardContainer from './components/KanbanBoardContainer';
+import KanbanBoard from './components/KanbanBoard';
+import EditCard from './components/EditCard';
+import NewCard from './components/NewCard';
 
 
 
-ReactDOM.render(<KanbanBoardContainer />, document.getElementById('app'));
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route component={KanbanBoardContainer}>
+			<Route path='/' component={KanbanBoard}>
+				<Route path='new' component={NewCard}/>
+				<Route path='edit/:card_id' component={EditCard}/>
+			</Route>
+		</Route>
+	</Router>
+), document.getElementById('app'));
